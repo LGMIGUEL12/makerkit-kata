783e60c7766a7cac33054a0b5a6d2ffc32bd156f
luis miguel ramos <ramosvasquezluismiguel@gmail.com>
2023-03-06 15:03:42 -0600

    se creo todo el input

--- app/components/tasks/TaskInputsContainer.tsx
+++ app/components/tasks/TaskInputsContainer.tsx
@@ -1,12 +1,15 @@
+import { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
-import { Trans } from 'react-i18next';
+import toast from 'react-hot-toast';
+import { Trans, useTranslation } from 'react-i18next';
 import Button from '~/core/ui/Button';
 import TextField from '~/core/ui/TextField';
 import useCreateTask from '~/lib/tasks/hooks/use-create-task';
 
 const TaskInputsContainer: React.FC<{}> = () => {
-  // const [createTask, createTaskState] = useCreateTask();
-
+  const [createTask, createTaskState] = useCreateTask();
+  const { loading } = createTaskState;
+  const { t } = useTranslation();
   const { register, handleSubmit, reset } = useForm({
     defaultValues: {
       title: '',
@@ -17,9 +20,22 @@ const TaskInputsContainer: React.FC<{}> = () => {
   const titleControl = register('title', { required: true });
   const descriptionControl = register('description', { required: true });
 
+  const onError = useCallback(() => {
+    toast.error('Error: please fill in all the required fields');
+  }, []);
+
   const onSubmit = async (title: string, description: string) => {
     console.log(title);
     console.log(description);
+
+    if (!title || !description) {
+      return onError();
+    }
+    await toast.promise(createTask(title, description), {
+      success: t<string>(`task:createTaskSuccess`),
+      error: t<string>(`task:createTaskError`),
+      loading: t<string>(`task:createTaskLoading`),
+    });
   };
 
   return (
@@ -33,7 +49,6 @@ const TaskInputsContainer: React.FC<{}> = () => {
           <TextField>
             <TextField.Label>
               <Trans i18nKey={'common:titleFormTask'} />
-
               <TextField.Input
                 name={titleControl.name}
                 innerRef={titleControl.ref}
@@ -48,7 +63,6 @@ const TaskInputsContainer: React.FC<{}> = () => {
           <TextField>
             <TextField.Label>
               <Trans i18nKey={'common:descriptionFormTask'} />
-
               <TextField.Input
                 name={descriptionControl.name}
                 innerRef={descriptionControl.ref}
@@ -65,6 +79,7 @@ const TaskInputsContainer: React.FC<{}> = () => {
             <Button
               className={'w-full md:w-auto'}
               data-cy={'create-task-submit-button'}
+              loading={loading}
             >
               <Trans i18nKey={'common:buttonCreateTask'} />
             </Button>
